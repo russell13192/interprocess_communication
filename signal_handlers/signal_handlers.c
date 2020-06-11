ec9e43c4f3222440cf7dbda86107d305a4dbfb3b
russell13192 <murrayge@mnstate.edu>
2020-06-11 15:26:26 -0700

    Init Commit

--- signal_handlers/signal_handlers.c
+++ signal_handlers/signal_handlers.c
@@ -2,3 +2,35 @@
 // Created by russell on 12/3/19.
 //
 
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void
+ctrlC_signal_handler(int sig)
+{
+    printf("Ctrl-c pressed\n");
+    printf("Bye Bye\n");
+    exit(0);
+}
+
+static void
+abort_signal_handler(int sig)
+{
+    printf("process is aborted\n");
+    printf("Bye Bye\n");
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    signal(SIGINT, ctrlC_signal_handler);
+    signal(SIGABRT, abort_signal_handler);
+    char ch;
+    printf("Abort process (y/n) ?\n");
+    scanf("%c", &ch);
+    if (ch == 'y')
+        abort();
+
+    return 0;
+}
